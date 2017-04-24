bb9b7b9d181c5bc570ba8487fdb77dcb9fff5f9b
Lauren Baker <laurenbaker@Laurens-MBP.mshome.net>
2017-04-24 12:04:30 -0600

    Set up structure and prototype.

--- Library-Lauren.js
+++ Library-Lauren.js
@@ -1,20 +1,47 @@
+
+function test(){
+  console.log("hit");
+};
+
+
 function newBook (title, author, numberOfPages, publisher){
   this.title = title;
   this.author = author;
   this.numberOfPages = numberOfPages;
   this.publisher = publisher;
-  }
+};
 
-  var library (){
+  var library = function() {
 
-  }
+  };
 
 library.prototype.bookArray = new Array();
-library.prototype.addBook = //add later
-Library.prototype.removeBook = //add later
-Library.prototype.getRandomBook = //add later
-Library.prototype.getBookByTitle = //add later
-Library.prototype.getBooksByAuthor = //add later
-Library.prototype.addBooks = //add later
-Library.prototype.getAuthors = //add later
-Library.prototype.getRandomAuthorName = //add later
+library.prototype.addBook = function(){
+
+};
+library.prototype.removeBook =  function(){
+
+};
+library.prototype.getRandomBook = function(){
+
+};
+library.prototype.getBookByTitle = function(){
+
+};
+library.prototype.getBooksByAuthor = function(){
+
+};
+library.prototype.addBooks = function(){
+
+};
+library.prototype.getAuthors = function(){
+
+};
+library.prototype.getRandomAuthorName = function(){
+
+};
+
+var book1 = new newBook ("Catcher in the Rye","J.D Salinger", "309", "Random House")
+
+
+var gLibrary = new library();
