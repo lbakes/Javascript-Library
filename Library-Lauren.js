61810ef3f8b670cd4c8bf15a67aceb96b969eca7
Lauren Baker <laurenbaker@Laurens-MBP.mshome.net>
2017-04-24 11:32:54 -0600

    Stood up html and js

---
+++ Library-Lauren.js
@@ -0,0 +1,20 @@
+function newBook (title, author, numberOfPages, publisher){
+  this.title = title;
+  this.author = author;
+  this.numberOfPages = numberOfPages;
+  this.publisher = publisher;
+  }
+
+  var library (){
+
+  }
+
+library.prototype.bookArray = new Array();
+library.prototype.addBook = //add later
+Library.prototype.removeBook = //add later
+Library.prototype.getRandomBook = //add later
+Library.prototype.getBookByTitle = //add later
+Library.prototype.getBooksByAuthor = //add later
+Library.prototype.addBooks = //add later
+Library.prototype.getAuthors = //add later
+Library.prototype.getRandomAuthorName = //add later
