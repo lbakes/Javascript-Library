4084e35572b38b57f00ee5492d087850724ea26e
Lauren Baker <laurenbaker@Laurens-MacBook-Pro.local>
2017-05-05 04:54:18 -0600

    Add book, add books, and Gets (without input) are all working. Get buttons with input are glitchy & need work. Have not started Removes.

--- Library.js
+++ Library.js
@@ -20,6 +20,49 @@ var aBooks = [
   book9
 ]
 
+library.prototype._addLi = function(){
+this.$dropDown.append("<li class='listGroupItem'>" + "<input type='text' class='title' placeholder='Title of Book'> <input type='text' class='author' placeholder='Author of Book'><input type='text' class='pages' placeholder='Number of Pages'><input type='text' class='date' placeholder='Publish Date'>" + "</li>")
+}
+
+
+library.prototype.init=function(){
+  this.$addBookButton=$("button.addBook");
+  this.$removebyTitle=$("button.rmveBkByTle");
+  this.$bookContainer=$("#bookContainer ul");
+  this.$ranBookBtn=$("button.getRanBk");
+  this.$ranAuthBtn=$("button.getRanAuth");
+  this.$getAuthors=$("button.getAuths");
+  this.$getBookByTitleBtn=$("button.getBookByTitleBtn");
+  this.$getBookByAuthor=$("button.getBookByAuth");
+  this.$dropDown=$("#dropDown ul");
+  this._bindEvents();
+};
+
+library.prototype._bindEvents = function(){
+  this.$addBookButton.on("click", $.proxy(this._getAddBookInputs, this));
+  this.$removebyTitle.on("click", $.proxy(this._getRemoveTitleInputs, this));
+  this.$ranBookBtn.on("click", $.proxy(this.getRandomBook, this));
+  this.$ranAuthBtn.on("click", $.proxy(this.getRandomAuthorName, this));
+  this.$getAuthors.on("click", $.proxy(this.getAuthors, this));
+  this.$getBookByTitleBtn.on("click", $.proxy(this.getBookByTitle, this));
+  this.$getBookByAuthor.on("click", $.proxy(this.getBookByAuthor, this));
+  $('#dropDownBtn').on("click", $.proxy(this._addLi, this));
+};
+
+library.prototype._getAddBookInputs = function(){
+  var _self=this;
+  $.each($("li"), function(index, value){
+    var title = $(this).children(".title").val();
+    var author = $(this).children(".author").val();
+    var pages = $(this).children(".pages").val();
+    var date = $(this).children(".date").val();
+    if(title && author && pages && date) {
+      var book = new newBook(title, author, pages, date);
+      _self.addBook(book);
+    }
+  });
+};
+
 function newBook (title, author, numberOfPages, publishDate){
   this.title = title;
   this.author = author;
@@ -30,30 +73,24 @@ function newBook (title, author, numberOfPages, publishDate){
 
 library.prototype.bookArray = [];
 
-// Purpose: Add a book object to your books array.
-// Return: boolean true if it is not already added, false if it is already added.
-
 library.prototype.addBook = function(book){
   for(var i = 0; i < this.bookArray.length; i++){
-  if  (this.bookArray[i].title == title) {
+  if  (this.bookArray[i].title == book.title) {
     return false;
   }
 }
     this.bookArray.push(book);
-    $("div.book-container").append("<li>" + book.title + " " + book.author + " " + book.numberOfPages + " " + book.publishDate + " " + "</li>");
+    $("div.bookContainer").append("<li>" + book.title + " " + book.author + " " + book.numberOfPages + " " + book.publishDate + " " + "</li>");
     return true;
 };
 
-// Purpose: Remove book from from the books array by its title.
-// Return: boolean true if the book(s) were removed, false if no books match
-
-library.prototype.removeBookByTitle = function (){
+library.prototype.removeBookByTitle = function (title){
   var regEx = new RegExp(title, 'i');
   var bool = false;
     for(var i = 0; i < this.bookArray.length; i++){
     if  (this.bookArray[i].title.match(regEx)) {
          this.bookArray.splice(i,1);
-         $("div.book-container").remove("<li>" + book.title + " " + book.author + " " + book.numberOfPages + " " + book.publishDate + " " + "</li>")
+         $("div.bookContainer").remove(book.title);
          bool = true;
 
 
@@ -61,9 +98,6 @@ library.prototype.removeBookByTitle = function (){
   }
         return bool;
 };
-
-// Purpose: Remove a specific book from your books array by the author name.
-// Return: boolean true if the book(s) were removed, false if no books match
 
 library.prototype.removeBookByAuthor = function (author){
   var regEx = new RegExp(author, 'i');
@@ -78,48 +112,39 @@ library.prototype.removeBookByAuthor = function (author){
       return bool;
 };
 
-
-// Purpose: Return a random book object from your books array
-// Return: book object if you find a book, null if there are no books
-
 library.prototype.getRandomBook = function(){
   if  (this.bookArray.length) {
-  var randomNum = Math.floor(Math.random() * this.bookArray.length);
-  return this.bookArray[randomNum];
+  var randomBook = this.bookArray[Math.floor(Math.random() * this.bookArray.length)];
+  $("div.sideBookContainer").append("<li>" + randomBook.title + " " + randomBook.author + " " + randomBook.numberOfPages + " " + randomBook.publishDate +"</li>");
+  return randomBook.title;
   }
     return null;
   };
 
-// Purpose: Return all books that completely or partially matches the string title passed into the function
-// Return: array of book objects if you find books with matching titles, empty array if no books are found
-
 library.prototype.getBookByTitle = function(title){
-  var regEx = new RegExp(title, 'i');
+  var regEx = new RegExp(title, 'gi');
   var matchArray = [];
   for(var i = 0; i < this.bookArray.length; i++){
     if(this.bookArray[i].title.match(regEx)){
         matchArray.push(this.bookArray[i]);
+        $("div.sideBookContainer").append("<li>" + this.bookArray[i].title + " " + this.bookArray[i].author + " " + this.bookArray[i].numberOfPages + " " + this.bookArray[i].publishDate + "</li>");
+
     }
   }
   return matchArray;
 };
 
-// Purpose: Finds all books where the author’s name partially or completely matches the authorName argument passed to the function.
-// Return: array of books if you find books with match authors, empty array if no books match
-
 library.prototype.getBookByAuthor = function(author){
-  var regEx = new RegExp(author, 'i');
+  var regEx = new RegExp(author, 'gi');
   var matchArray = [];
   for(var i = 0; i < this.bookArray.length; i++){
     if(this.bookArray[i].author.match(regEx)){
         matchArray.push(this.bookArray[i]);
+        $("div.sideBookContainer").append("<li>" + this.bookArray[i].title + " " + this.bookArray[i].author + " " + this.bookArray[i].numberOfPages + " " + this.bookArray[i].publishDate + "</li>");
     }
   }
   return matchArray;
 };
-
-// Purpose: Takes multiple books, in the form of an array of book objects, and adds the objects to your books array.
-// Return: number of books successfully added, 0 if no books were added
 
 library.prototype.addBooks = function(array1){
 var num = 0;
@@ -130,57 +155,26 @@ for(var i = 0; i < array1.length; i++){
   }
 return num;
 };
-// Purpose: Find the distinct authors’ names from all books in your library
-// Return: array of strings the names of all distinct authors, empty array if no books exist or if no authors exist
 
 library.prototype.getAuthors = function(){
-  var authorsArray = [];
+  var authorsArray = new Array;
   for(var i = 0; i < this.bookArray.length; i++){
     if (authorsArray.indexOf(this.bookArray[i].author) < 0){
      authorsArray.push(this.bookArray[i].author);
     }
   }
+  $("div.sideBookContainer").append("<li>" + authorsArray + "</li>");
   return authorsArray;
 };
 
-// Purpose: Retrieves a random author name from your books collection
-// Return: string author name, null if no books exist
-
 library.prototype.getRandomAuthorName = function(){
   if  (this.bookArray.length > 0) {
-  var randomNum = Math.floor(Math.random() * this.bookArray.length);
-  return this.bookArray[randomNum].author;
+  var randomBook = this.bookArray[Math.floor(Math.random() * this.bookArray.length)];
+    $("div.sideBookContainer").append("<li>" + randomBook.author + "</li>");
+    return randomBook;
     }
     return null;
   };
-
-
-  library.prototype.init=function(){
-    this.$addBookButton=$("button.add-bk");
-    this.$rmveBookByTleButton=$("button.rmve-bk-by-tle")
-    this._bindEvents();
-  };
-
-  library.prototype._bindEvents = function(){
-    this.$addBookButton.on("click", $.proxy(this._getForms, this));
-    this.$rmveBookByTleButton.on("click", $.proxy(this._getForm1, this));
-  };
-
-  library.prototype._getForms= function(){
-    var _self=this;
-    $.each($("form"), function(index, value){
-      var title = $(this).children(".title").val();
-      var author = $(this).children(".author").val();
-      var pages = $(this).children(".pages").val();
-      var date = $(this).children(".date").val();
-      if(title && author && pages && date) {
-        var book = new newBook(title, author, pages, date)
-        _self.addBook(book);
-      }
-    });
-  };
-
-
 
 
 var gLibrary = new library();
